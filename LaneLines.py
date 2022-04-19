5934fb5689a94fc23fe40acfda98a22c4b5c30ef
eslammagdymohamed <em336606@gmail.com>
2022-04-19 16:55:04 +0200

    Add function fit_poly

--- LaneLines.py
+++ LaneLines.py
@@ -37,3 +37,29 @@ class LaneLines:
         self.margin = 0
         # Mininum number of pixels found to recenter window
         self.minpix = 0
+
+
+    def fit_poly(self, img):
+        leftx, lefty, rightx, righty, out_img = self.find_lane_pixels(img)
+      
+        if len(lefty) > 1200:
+            self.left_fit = np.polyfit(lefty, leftx, 2)
+        if len(righty) > 1200:
+            self.right_fit = np.polyfit(righty, rightx, 2)
+
+        # Generate x and y values for plotting
+        maxy = img.shape[0] - 1
+        miny = img.shape[0] // 3
+ 
+        if len(lefty):
+            maxy = max(maxy, np.max(lefty))
+            miny = min(miny, np.min(lefty))
+
+        if len(righty):
+            maxy = max(maxy, np.max(righty))
+            miny = min(miny, np.min(righty))
+
+        ploty = np.linspace(miny, maxy, img.shape[0])
+
+        left_fitx = self.left_fit[0]*ploty**2 + self.left_fit[1]*ploty + self.left_fit[2]
+        right_fitx = self.right_fit[0]*ploty**2 + self.right_fit[1]*ploty + self.right_fit[2]    
