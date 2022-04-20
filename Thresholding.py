4fc5dd3019f6067a83a8be52a6c5779af41828d6
Addo <1700759@eng.asu.edu.eg>
2022-04-20 15:56:37 +0200

    Thresholding

---
+++ Thresholding.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+def threshold_rel(img, lo, hi):
+    vmin = np.min(img)
+    vmax = np.max(img)
+    
+    vlo = vmin + (vmax - vmin) * lo
+    vhi = vmin + (vmax - vmin) * hi
+    return np.uint8((img >= vlo) & (img <= vhi)) * 255
+
+def threshold_abs(img, lo, hi):
+    return np.uint8((img >= lo) & (img <= hi)) * 255
