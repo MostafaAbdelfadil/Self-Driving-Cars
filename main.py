b2ea9da7ec7040818d71415da30550f58279fa0b
MostafaAbdelfadil <mostafaabdelfadil81@gmail.com>
2022-04-20 14:58:55 +0200

    input and output arguments

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.image as mpimg
 import cv2
+from docopt import docopt
+from IPython.display import HTML, Video
+from moviepy.editor import VideoFileClip
+from CameraCalibration import CameraCalibration
+from Thresholding import *
+from PerspectiveTransformation import *
+from LaneLines import *
+
 
 class FindLaneLines:
     def __init__(self):
-
+        
     def forward(self, img):
 
 
@@ -12,7 +20,15 @@ class FindLaneLines:
 
 
 def main():
-    
+    args = docopt(__doc__)
+    input = args['INPUT_PATH']
+    output = args['OUTPUT_PATH']
+
+    findLaneLines = FindLaneLines()
+    if args['--video']:
+        findLaneLines.process_video(input, output)
+    else:
+        findLaneLines.process_image(input, output)
 
 if __name__ == "__main__":
     main()
